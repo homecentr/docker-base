27afd238f782ddeca3f10824d9ade328cccfa763
Lukas Holota <l.holota@outlook.com>
2020-03-13 15:24:36 +0100

    Support for explicit root PUID and PGID

--- etc/cont-init.d/10-init.sh
+++ etc/cont-init.d/10-init.sh
@@ -2,14 +2,29 @@
 PUID=${PUID:-7077}
 PGID=${PGID:-7077}
 
-if ( ! type "groupadd" > /dev/null;) || (! type "useradd" > /dev/null; )
+DISPLAY_GROUP="root"
+DISPLAY_USER="root"
+
+if [ "$PGID" != "0" ] || [ "$PUID" != "0" ]
 then
-   >&2 echo "Cannot create user, please install shadow package!"
-   exit 1
+  if ( ! type "groupadd" > /dev/null; ) || ( ! type "useradd" > /dev/null; )
+  then
+    >&2 echo "Cannot create user/group, please install shadow package!"
+    exit 1
+  fi
 fi
 
-addgroup -g $PGID nonroot
-adduser -u $PUID -G nonroot -D nonroot
+if [ "$PGID" -ne "0" ]
+then
+  addgroup -g $PGID nonroot
+  DISPLAY_GROUP="nonroot"
+fi
+
+if [ "$PUID" -ne "0" ]
+then
+  adduser -u $PUID -G nonroot -D nonroot
+  DISPLAY_USER="nonroot"
+fi
 
 echo '
     __  __                                     __
@@ -20,7 +35,7 @@ echo '
 '
 echo "
 -------------------------------------
-User uid:    $(id -u nonroot)
-User gid:    $(id -g nonroot)
+User uid:    $(id -u $DISPLAY_USER)
+User gid:    $(id -g $DISPLAY_GROUP)
 -------------------------------------
 "
